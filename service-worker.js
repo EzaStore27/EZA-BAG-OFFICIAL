95c8a65503dc783feabf208785acd91f3748e712
EzaStore27 <ahmad_syai@yahoo.co.id>
2026-05-07 22:58:45 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,72 @@
-const CACHE_NAME = 'eza-bag-v1';
+const CACHE_NAME = 'eza-bag-v2';
 
 const urlsToCache = [
   '/',
-  '/index.html'
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
+// INSTALL
 self.addEventListener('install', event => {
 
+  self.skipWaiting();
+
   event.waitUntil(
+
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+
+        return cache.addAll(urlsToCache);
+
+      })
+
   );
 
 });
 
+// ACTIVATE
+self.addEventListener('activate', event => {
+
+  event.waitUntil(
+
+    caches.keys().then(cacheNames => {
+
+      return Promise.all(
+
+        cacheNames.map(cache => {
+
+          if(cache !== CACHE_NAME){
+
+            return caches.delete(cache);
+
+          }
+
+        })
+
+      );
+
+    })
+
+  );
+
+  return self.clients.claim();
+
+});
+
+// FETCH
 self.addEventListener('fetch', event => {
 
   event.respondWith(
+
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+
+        return response || fetch(event.request);
+
+      })
+
   );
 
 });
